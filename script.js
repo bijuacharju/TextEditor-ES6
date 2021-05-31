ee2f1222a5e1774e5a8706a283d9809ceb7d0c16
Biju Acharju <78154490+bijuacharju@users.noreply.github.com>
2021-05-31 16:20:23 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,18 @@
-/**
- * Update the output text as you type in the textarea
- */
 updateText = () => {
   let text = document.getElementById("text-input").value;
   document.getElementById('text-output').innerText = text;
 }
 
-/**
- * Toggle the bold class for the output text
- */
 makeBold = (elem) => {
   elem.classList.toggle('active');
   document.getElementById('text-output').classList.toggle('bold');
 }
 
-/**
- * Toggle the italic class for the output text
- */
 makeItalic = (elem) => {
   elem.classList.toggle('active');
   document.getElementById('text-output').classList.toggle('italic');
 }
 
-/**
- * Toggle the underline class for the output text
- */
 makeUnderline = (elem) => {
   elem.classList.toggle('active');
   let output = document.getElementById('text-output');
@@ -35,10 +23,6 @@ makeUnderline = (elem) => {
   }
 }
 
-/**
- * Toggle the style textAlign attribute
- * Toggle the active state for the align butttons
- */
 alignText = (elem, alignType) => {
   document.getElementById('text-output').style.textAlign = alignType;
   let alignButtons = document.getElementsByClassName('align');
